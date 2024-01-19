4361667b449e72e40431a4d8b743e205969ffd93
PritamKumarSaha <137249952+PritamKumarSaha@users.noreply.github.com>
2024-01-19 14:19:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,10 +6,12 @@ function Car(make, model) {
 Car.prototype.getMakeModel = function() {
 	return `${this.make} ${this.model}`;
 };
-
+// Constructor function for SportsCar
 function SportsCar(make, model, topSpeed) {
+	// call the Car constructor to initialize make and model
 	Car.call(this, make, model);
-	this.topSpeed = topSpeed;
+	// additional property for topSpeed
+	this._topSpeed = topSpeed;
 };
 SportsCar.prototype = Object.create(Car.prototype);
 SportsCar.prototype.getTopSpeed = function() {
